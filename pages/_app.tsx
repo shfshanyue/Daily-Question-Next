c24646007a887c236eeb4cf4b44172eb0797b539
shfshanyue <xianger94@gmail.com>
2023-04-11 21:29:27 +0800

    chore: update home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Helmet
         htmlAttributes={{ lang: 'en' }}
-        title="Hello next.js!"
+        title="定制你的 ChatGPT 机器人"
         meta={meta}
       />
       <Component {...pageProps} />
