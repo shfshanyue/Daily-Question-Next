a4d7b5f2c238d4ffb106c04ebac9dd63c6f588b5
shfshanyue <xianger94@gmail.com>
2023-06-21 13:20:59 +0800

    chore: update react-ga4

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,11 +1,11 @@
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
 
 export const initGA = () => {
   // 设置 GA code
-  ReactGA.initialize(process.env.gaId || '')
+  ReactGA.initialize('102193749')
 }
 
 export const logPageView = () => {
   ReactGA.set({ page: window.location.pathname })
-  ReactGA.pageview(window.location.pathname)
+  ReactGA.send({ hitType: 'pageview', page: window.location.pathname, title: window.document.title });
 }
